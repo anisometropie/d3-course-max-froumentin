ec60ba6c79d982586bcd5afa5ba9ebd09c4ef598
Valentin Chanas <valentin.chanas@dcbrain.com>
2019-02-09 19:25:03 +0100

    better structure

--- main.js
+++ main.js
@@ -1,61 +1,47 @@
-var myGroup = d3.select('#chart')
-  .append('g')
-  .attr('class', 'myGroup');
+const axisWidth = 400;
+const axisHeight = 300;
 
-/*
-myGroup
-  .append('circle')
-  .attr('cx', 200)
-  .attr('cy', 250)
-  .attr('r', 50)
-  .on('click', function() {
-    myGroup.select('rect').attr('fill', 'blue');
+const scaleX = d3
+  .scaleLinear()
+  .domain([-100, 100])
+  .range([0, axisWidth]);
+const axisX = d3.axisBottom(scaleX).ticks(10);
+
+const scaleY = d3
+  .scaleLinear()
+  .domain([0, 100])
+  .range([axisHeight, 0]);
+const axisY = d3.axisLeft(scaleY).ticks(10);
+
+const chart = d3
+  .select("#chart")
+  .append("g")
+  .attr("transform", "translate(50,100)");
+
+const axes = chart.append("g");
+axes
+  .append("g")
+  .attr("transform", `translate(0,${axisHeight})`)
+  .call(axisX);
+axes.append("g").call(axisY);
+
+const figures = chart
+  .append("g")
+  .attr("class", "figures")
+  .attr("transform", `translate(0,${axisHeight})`);
+
+figures
+  .append("circle")
+  .attr("cx", 40)
+  .attr("cy", -40)
+  .attr("r", 25)
+  .on("click", function() {
+    figures.select("rect").attr("fill", "blue");
   });
 
-
-myGroup
-  .append('rect')
-  .attr('x', 30)
-  .attr('y', 40)
-  .attr('width',200)
-  .attr('height', 200);
-*/
-
-var scale = d3.scaleLinear()
-  .domain([-100, 100])
-  .range([0, 400]);
-
-var axis = d3.axisBottom(scale)
-  .ticks(10);
-
-d3.select('#chart')
-  .append('g')
-    .attr('transform', 'translate(50,400)')
-    .call(axis);
-
-
-var scaleY = d3.scaleLinear()
-  .domain([100, 0])
-  .range([0, 300]);
-
-var axisY = d3.axisLeft(scaleY)
-  .ticks(10);
-
-d3.select('#chart')
-  .append('g')
-    .attr('transform', 'translate(50, 100)')
-    .call(axisY);
-
-myGroup
-  .append('rect')
-    .attr('x', 50)
-    .attr('y', 250)
-    .attr('width',100)
-    .attr('height',150);
-
-myGroup
-  .append('rect')
-    .attr('x', 150)
-    .attr('y', 200)
-    .attr('width',100)
-    .attr('height',200);
+figures
+  .append("rect")
+  .attr("x", 30)
+  .attr("y", -42)
+  .attr("width", 40)
+  .attr("height", 25);
